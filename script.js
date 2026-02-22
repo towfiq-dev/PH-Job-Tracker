5e2ac38851acaf33c0041c8ce25f09c3ab074ea3
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-02-22 11:48:37 +0600

    added all ids variable

---
+++ script.js
@@ -0,0 +1,7 @@
+//Keep all IDs in variables
+const mainContainer = document.getElementById('job-container');
+const tCount = document.getElementById('total-count');
+const iCount = document.getElementById('interview-count');
+const rCount = document.getElementById('rejected-count');
+const tabText = document.getElementById('tab-job-count');
+const noData = document.getElementById('empty-state');
